3a3ddfe25f22b888ebe15a7ed2000caebcaca082
CESAR PAOLO GALICIA MORALES <89037463+CESARGALICIA33@users.noreply.github.com>
2023-11-13 16:10:35 -0600

    V 1.0.1
    Actualización en el apartado de login y registro de candidatos, validacion de jwt completa y funcionando con la base de datos, registro de candidatos con envio de documento pdf.

--- APIproyectofinal/api-rest/Login.php
+++ APIproyectofinal/api-rest/Login.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once ('../includes/AuthToken.php');
-
+require_once ('../includes/Database.class.php');
+/*
 // Obtener datos del cuerpo de la solicitud
 $data = json_decode(file_get_contents("php://input"));
 
@@ -20,6 +21,62 @@ if ($data->correo === 'usuario@ejemplo.com' && $data->contrasena === 'contrasena
     header('Content-Type: application/json');
     http_response_code(401); // No autorizado
     echo json_encode(['success' => false, 'message' => 'Credenciales incorrectas']);
+}*/
+
+// Obtener datos del cuerpo de la solicitud
+$data = json_decode(file_get_contents("php://input"));
+
+// Verificar si se proporcionaron datos
+if (isset($data->correo) && isset($data->contrasena) && isset($data->accountType)) {
+    // Obtener los datos del usuario desde la base de datos
+    $correo = $data->correo;
+    $contrasena = $data->contrasena;
+    $accountType = $data->accountType;
+
+    $user = getUserFromDatabase($correo, $contrasena, $accountType);
+
+    // Verificar las credenciales
+    if ($user) {
+        // Usuario autenticado, generamos un token
+        $userId = $user['ID']; // Reemplaza con el campo real del ID del usuario
+        $token = AuthToken::generateToken($userId, $accountType);
+
+        // Devolvemos el token en la respuesta
+        header('Content-Type: application/json');
+        echo json_encode(['success' => true, 'token' => $token]);
+    } else {
+        // Usuario no autenticado
+        header('Content-Type: application/json');
+        http_response_code(401); // No autorizado
+        echo json_encode(['success' => false, 'message' => 'Credenciales incorrectas']);
+    }
+} else {
+    // Datos incompletos en la solicitud
+    header('Content-Type: application/json');
+    http_response_code(400); // Solicitud incorrecta
+    echo json_encode(['success' => false, 'message' => 'Datos incompletos en la solicitud']);
+}
+
+// Función para obtener usuario desde la base de datos
+function getUserFromDatabase($correo, $contrasena, $accountType) {
+    $conexion = new Conexion();
+
+    try {
+        $query = ($accountType == 2)
+            ? "SELECT * FROM empresa WHERE Nombre = :correo AND Contraseña = :contrasena"
+            : "SELECT * FROM usuario WHERE Correo = :correo AND Contraseña = :contrasena";
+
+        $statement = $conexion->prepare($query);
+        $statement->bindParam(':correo', $correo);
+        $statement->bindParam(':contrasena', $contrasena);
+        $statement->execute();
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        header('HTTP/1.1 500 Internal Server Error');
+        echo json_encode(['error' => 'Error en la base de datos: ' . $e->getMessage()]);
+        exit;
+    }
 }
 
 ?>
--- APIproyectofinal/includes/Client.class.php
+++ APIproyectofinal/includes/Client.class.php
@@ -29,7 +29,7 @@
         
         }
 
-        public static function insertarUsuario($correo, $contrasena)
+        /*public static function insertarUsuario($correo, $contrasena)
         {
             $conexion = new Conexion();
 
@@ -50,7 +50,38 @@
                     echo json_encode($success);
                 }
             
+        }*/
+
+        public static function insertarUsuario($correo, $contrasena)
+{
+    $conexion = new Conexion();
+
+    try {
+        $query = "INSERT INTO usuario (Correo, Contraseña) VALUES (:correo, :contrasena)";
+        $statement = $conexion->prepare($query);
+        $statement->bindParam(':correo', $correo);
+        $statement->bindParam(':contrasena', $contrasena);
+        $statement->execute();
+
+        if ($statement->rowCount() > 0) {
+            // Usuario creado correctamente
+            $usuarioId = $conexion->lastInsertId(); // Obtener el ID del usuario recién insertado
+            $success = true;
+            header('HTTP/1.1 201 Usuario creado correctamente');
+            echo json_encode([ 'userId' => $usuarioId]);
+        } else {
+            // Error al crear el usuario
+            $success = false;
+            header('HTTP/1.1 404 Usuario no creado correctamente');
+            echo json_encode(['success' => $success]);
         }
+    } catch (PDOException $e) {
+        // Manejar errores de la base de datos
+        header('HTTP/1.1 500 Internal Server Error');
+        echo json_encode(['error' => 'Error en la base de datos: ' . $e->getMessage()]);
+    }
+}
+
 
 
         public static function insertarAreaPost($nombre, $descripcion)
@@ -75,7 +106,7 @@
                 }
             
         }
-
+            //eliminar clase
         public static function validarCredenciales($correo, $contrasena, $accountType)
         {
             $conexion = new Conexion();
@@ -106,6 +137,51 @@
                 echo json_encode(array('error' => 'Error en la base de datos: ' . $e->getMessage()));
             }
         }
+
+
+        public static function InsertarCandidato($nombre, $apellido_materno, $apellido_paterno, $genero, $telefono, $calle, $colonia, $num_int, $num_ext, $codigoPostal, $experiencia, $educacion, $habilidades, $fecha_nacimiento, $curriculumContent, $constanciaContent, $disponibilidad, $salario, $idusuario)
+        {
+            $conexion = new Conexion();
+        
+            $query = "INSERT INTO candidato (Nombre, ApellidoPaterno, ApellidoMaterno, Género, Teléfono, Calle, Colonia, NumInt, NumExt, CódigoPostal, ExperienciaLaboral, Educación, Habilidades, FechaNacimiento, CurriculumVitae, Constancia, Disponibilidad, Salario, IdUsuario) VALUES (:nombre, :apellido_materno, :apellido_paterno, :genero, :telefono, :calle, :colonia, :num_int, :num_ext, :codigoPostal, :experiencia, :educacion, :habilidades, :fecha_nacimiento, :curriculum, :contancia, :disponibilidad, :salario, :idusuario)";
+        
+            $statement = $conexion->prepare($query);
+            $statement->bindParam(':nombre', $nombre);
+            $statement->bindParam(':apellido_paterno', $apellido_paterno);
+            $statement->bindParam(':apellido_materno', $apellido_materno);
+            $statement->bindParam(':genero', $genero);
+            $statement->bindParam(':telefono', $telefono);
+            $statement->bindParam(':calle', $calle);
+            $statement->bindParam(':colonia', $colonia);
+            $statement->bindParam(':num_int', $num_int);
+            $statement->bindParam(':num_ext', $num_ext);
+            $statement->bindParam(':codigoPostal', $codigoPostal);
+            $statement->bindParam(':experiencia', $experiencia);
+            $statement->bindParam(':educacion', $educacion);
+            $statement->bindParam(':habilidades', $habilidades);
+            $statement->bindParam(':fecha_nacimiento', $fecha_nacimiento);
+            $statement->bindParam(':curriculum', $curriculumContent, PDO::PARAM_LOB);
+            $statement->bindParam(':contancia', $constanciaContent, PDO::PARAM_LOB);
+            $statement->bindParam(':disponibilidad', $disponibilidad);
+            $statement->bindParam(':salario', $salario);
+            $statement->bindParam(':idusuario', $idusuario);
+        
+            $statement->execute();
+        
+            if ($statement->rowCount() > 0) {
+                $success = true;
+                header('HTTP/1.1 201 Candidato creada correctamente');
+                echo json_encode($success);
+            } else {
+                $success = false;
+                header('HTTP/1.1 404 Candidato no creada correctamente');
+                echo json_encode($success);
+            }
+        }
+        
+        
+
+
 
 
     }
